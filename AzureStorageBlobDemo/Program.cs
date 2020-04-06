97897e8213c36e66ea1f90cc40472ab937591c70
Emaad Paracha <emaadparacha@hotmail.com>
2020-04-06 02:00:56 -0400

    Project files uploaded

---
+++ AzureStorageBlobDemo/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace AzureStorageBlobDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create Reference to Azure Storage Account
+            String strorageconn = System.Configuration.ConfigurationSettings.AppSettings.Get("StorageConnectionString");
+            CloudStorageAccount storageacc = CloudStorageAccount.Parse(strorageconn);
+
+            //Create Reference to Azure Blob
+            CloudBlobClient blobClient = storageacc.CreateCloudBlobClient();
+
+            //The next 2 lines create if not exists a container named "democontainer"
+            CloudBlobContainer container = blobClient.GetContainerReference("democontainer");
+
+            container.CreateIfNotExists();
+
+
+            //The next lines extract metadata from the file and upload it with the name DemoBlob on the container "democontainer"
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("DemoUpload");
+
+            using (var filestream = System.IO.File.OpenRead(@"/Users/DemoUser/Desktop/sample_srt.srt"))
+            {
+                //Extracts the 4th line of the SRT file and extracts the date and time
+                string datetime = System.IO.File.ReadLines("/Users/DemoUser/Desktop/sample_srt.srt").Skip(3).Take(1).First();
+                string[] datetimewords = datetime.Split(' ');
+                string[] timewords = datetimewords[1].Split(',');
+
+                //Changes the date from numeric form to readable form (2019-02-28 to February 28, 2019)
+                string[] wordmonth = datetimewords[0].Split('-');
+                string month = wordmonth[1];
+                int monthnum = int.Parse(month);
+                System.Globalization.DateTimeFormatInfo mfi = new System.Globalization.DateTimeFormatInfo();
+                string monthname = mfi.GetMonthName(monthnum).ToString();
+
+                string datewords = monthname + " " + wordmonth[2] + ", " + wordmonth[0];
+
+                //Extracts the 5th line of the SRT file
+                string latlong = System.IO.File.ReadLines("/Users/DemoUser/Desktop/sample_srt.srt").Skip(4).Take(1).First();
+
+                //Extracts the latitude and longitude from the 5th line
+                string[] latlongspec = latlong.Split(']');
+                string[] lat = latlongspec[7].Split(' ');
+                string[] log = latlongspec[8].Split(' ');
+
+                //Sets up coordinate string
+                string coord = "(" + lat[2] + "," + log[2] + ")";
+                string coordnopar = lat[2] + "," + log[2];
+
+                //Creates a link that uses Azure Maps API to get cross street information from the given coordinates
+                string crossurl = "https://atlas.microsoft.com/search/address/reverse/crossStreet/json?subscription-key=<ADD-AZURE-MAPS-CONNECTION-STRING-HERE>&api-version=1.0&query=" + coordnopar;
+
+                //Reads the JSON file from the link and converts to it to string
+                var jsoncross = new WebClient().DownloadString(crossurl);
+                string jsoncrossstr = jsoncross.ToString();
+
+                //Extracts text from the JSON file
+                string[] crosss = jsoncross.Split('"');
+
+                string crossstreet = "";
+
+                //If cross street data was found, it extracts it. If not, the variable is assigned "N/A"
+                if (jsoncrossstr.Contains("streetName"))
+                {
+                    string[] streetnamesplit = jsoncrossstr.Split(new string[] { "streetName" }, StringSplitOptions.None);
+                    string[] streetnamesplit2 = streetnamesplit[1].Split('"');
+                    crossstreet = streetnamesplit2[2];
+                }
+
+                else
+                {
+                    crossstreet = "N/A";
+                }
+
+                //Metadata being added to the uploaded blobs
+                blockBlob.Metadata.Add("Date", datetimewords[0]);
+                blockBlob.Metadata.Add("Time", timewords[0]);
+                blockBlob.Metadata.Add("Coordinates", coord);
+                blockBlob.Metadata.Add("CrossStreet", crossstreet);
+                blockBlob.Metadata.Add("FullDate", datewords);
+
+                //Blob uploaded to Blob Storage
+                blockBlob.UploadFromStream(filestream);
+
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Azure Blob Storage Upload with Metadata
+
+This project uploads a file to Azure Blob Storage and extracts text from the file, parses it, and applies it as metadata in Blob Storage.
+
+A demo SRT file is provided, to test text extraction and metadata upload.
+
+Please change the StorageConnectionString in App.config in order to upload to the right storage account.
+Also please add an API Key for Azure Maps in Program.cs in "string crossurl" to connect to Azure Maps to get cross street info.
+
+This demo uploads the SRT file as "DemoUpload" to container "democontainer". These settings can also be changed.
+
+The date and time are extracted from the 4th line of the SRT file, and latitude and longitude are extracted from the 5th line. The date is also converted from numeric form to text form. The coordinates are fed into Azure Maps, and a JSON file is read where the corresponding cross street is calculated. At the end, metadata is applied to the blob and it is uploaded to Blob Storage.
+
